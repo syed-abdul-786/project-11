a9dc188db6b6905bda169ed345d913b1f4d64dcc
syed-abdul-786 <85748495+syed-abdul-786@users.noreply.github.com>
2021-06-25 20:05:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var ship,ship_running
+var sea,sea
+
+
+
+function preload(){
+ship_running=loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+
+
+
+seaImg = loadImage("sea.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+
+  sea = createSprite(200,200);
+  sea.addImage(seaImg);
+   
+   ship = createSprite(10,10,10,10);
+   ship.addAnimation("running",ship_running)
+}
+
+function draw() {
+  background("blue");
+
+  if(sea.x < 0){
+    sea.x = sea.width/2;
+  }
+
+
+
+ drawSprites();
+}
